43dec3a153e3ab3803fbd9afba4201727834448b
Gonzalo Torrecillas <gonzalo.tp97@gmail.com>
2020-07-31 20:55:37 +0200

    add 1.8

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build --build-arg PAPER_VERSION=? --build-arg PAPER_BUILD=latest --rm=true -t papermc:latest .
+docker build --build-arg PAPER_VERSION=1.8.8 --build-arg PAPER_BUILD=latest --rm=true -t papermc:latest .
